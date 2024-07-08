f2b477926496ce62550f1d14492b8aa3e20e4984
aknEvrnky <evrenkaya.2002.save@gmail.com>
2024-07-08 03:02:00 +0300

    update config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+func GetApplicationPort() int {
+	port, err := strconv.ParseInt(getEnvironmentValue("APP_PORT"), 10, 64)
+	if err != nil {
+		log.Fatalf("failed to parse APP_PORT: %v", err)
+	}
+
+	return int(port)
+}
+
+func getEnvironmentValue(key string) string {
+	var val string
+	if val = os.Getenv(key); val == "" {
+		log.Fatalf("environment variable %s is not set", key)
+	}
+
+	return val
+}
