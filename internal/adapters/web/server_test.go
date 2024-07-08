25b63428c5eb6b8d74f0039dac898e36bba8c74c
aknEvrnky <evrenkaya.2002.save@gmail.com>
2024-07-08 13:56:36 +0300

    add tests for server run & shutdown

--- internal/adapters/web/server_test.go
+++ internal/adapters/web/server_test.go
@@ -2,8 +2,11 @@ package web
 
 import (
 	"github.com/aknEvrnky/currency-api-hexogonal/internal/application/core/domain"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
+	"time"
 )
 
 type mockApiAdapter struct {
@@ -27,4 +30,59 @@ func TestItCanInitializeANewAdapter(t *testing.T) {
 	if adapter.port != 3000 {
 		t.Errorf("Expected port to be 3000, got %d", adapter.port)
 	}
+}
+
+func TestAdapter_Run(t *testing.T) {
+	// Create a new Fiber app
+	app := fiber.New()
+
+	// Mock ApiPort implementation
+	api := &mockApiAdapter{}
+
+	// Create a new Adapter with the mock ApiPort
+	adapter := NewAdapter(api, 3000)
+	adapter.FiberApp = app
+
+	var err error
+	// Run the server in a separate goroutine
+	go func() {
+		if err = adapter.Run(); err != nil {
+			t.Errorf("Error running the server: %v", err)
+		}
+	}()
+	defer adapter.Shutdown()
+
+	// Allow the server to start
+	<-time.After(100 * time.Millisecond)
+
+	assert.Nil(t, err)
+}
+
+func TestAdapter_Shutdown(t *testing.T) {
+	// Create a new Fiber app
+	app := fiber.New()
+
+	// Mock ApiPort implementation
+	api := &mockApiAdapter{}
+
+	// Create a new Adapter with the mock ApiPort
+	adapter := NewAdapter(api, 3000)
+	adapter.FiberApp = app
+
+	var err error
+	// Run the server in a separate goroutine
+	go func() {
+		if err = adapter.Run(); err != nil {
+			t.Errorf("Error running the server: %v", err)
+		}
+	}()
+
+	// Allow the server to start
+	<-time.After(100 * time.Millisecond)
+
+	assert.Nil(t, err)
+
+	// Shutdown the server
+	err = adapter.Shutdown()
+	assert.NoError(t, err)
 }
