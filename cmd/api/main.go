db4a0a54418c64670dc286af0d39f07e287e8a4f
aknEvrnky <evrenkaya.2002.save@gmail.com>
2024-07-08 03:02:41 +0300

    add application entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/aknEvrnky/currency-api-hexogonal/config"
+	"github.com/aknEvrnky/currency-api-hexogonal/internal/adapters/currency"
+	"github.com/aknEvrnky/currency-api-hexogonal/internal/adapters/web"
+	"github.com/aknEvrnky/currency-api-hexogonal/internal/application/core/api"
+)
 
+func main() {
+	currencyAdapter := currency.NewAdapter()
+
+	apiAdapter := api.NewApplication(currencyAdapter)
+
+	server := web.NewAdapter(apiAdapter, config.GetApplicationPort())
+
+	server.Run()
 }
